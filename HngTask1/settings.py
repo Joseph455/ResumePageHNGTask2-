c0437150a7f67b8db50257471470484ba56e220b
Joseph Adebowale <adebowalejojo2001@gmail.com>
2021-08-18 14:05:32 +0100

    commit

--- HngTask1/settings.py
+++ HngTask1/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', os.getenv('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['hng-resume-app.herokuapp.com']
 
 
 # Application definition
